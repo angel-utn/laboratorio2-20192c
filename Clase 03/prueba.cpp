8ed180cdf43778796ac1913666f27fa70d14d53d
Angel <mac@macbook.local>
2019-08-23 08:25:27 -0300

    Curso viernes

--- Clase 03/prueba.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-  cout << "Ejemplo de Git para los alumnos de Laboratorio II" << endl;
-  cout << "Nueva señal de wifi gracias a MAGNE" << endl;
-  return 0;
-}
